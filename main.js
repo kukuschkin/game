1348ba83789ebf826065e9b9a233f8d6cca83426
kukuschkin <andyfreaki@outlook.com>
2017-02-20 10:45:41 +0100

    Update main.js

--- main.js
+++ main.js
@@ -44,7 +44,7 @@ var mainState = {
         
         this.timer = game.time.events.loop(1500, this.addRowOfPipes, this); 
         
-        this.score = 0;
+        this.score = -1;
             this.labelScore = game.add.text(20, 20, "0", 
             { font: "30px Arial", fill: "#ffffff" });   
     },
